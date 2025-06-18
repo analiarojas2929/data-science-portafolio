eb3607ec2788932d0f7438dc9d1d452081cbdf51
Analiarojas2929 <analiaro1213@gmail.com>
2025-06-18 17:36:29 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Cambiado de 'dist' a 'build'
+    outDir: 'dist',  // Esto debe ser 'dist'
     emptyOutDir: true
   }
 });
